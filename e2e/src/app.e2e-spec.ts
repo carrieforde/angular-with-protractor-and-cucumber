3479987328a463b661efd88dc368481f8c2a79e1
Carrie Forde <cforde@bills.com>
2019-09-10 14:30:12 -0700

    Adds page object to test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,39 +1,34 @@
-import { browser, element, by } from 'protractor';
+import { browser } from 'protractor';
+import { CalculatorPage } from './app.po';
+
+const calc = new CalculatorPage();
 
 describe('Protractor demo app', () => {
-  const firstNumber = element(by.model('first'));
-  const secondNumber = element(by.model('second'));
-  const goButton = element(by.id('gobutton'));
-  const latestResult = element(by.binding('latest'));
 
-  beforeEach(() => {
-    browser.get('http://juliemr.github.io/protractor-demo/');
+  beforeEach(async () => {
+    await calc.navigateTo();
   });
 
   it('should have a title', () => {
     expect(browser.getTitle()).toEqual('Super Calculator');
   });
 
-  it('should add one and two', () => {
-    firstNumber.sendKeys(1);
-    secondNumber.sendKeys(2);
+  it('should add one and two', async () => {
+    await calc.setValues(1, 2);
+    calc.goButton.click();
 
-    goButton.click();
-
-    expect(latestResult.getText()).toEqual('3');
+    expect(calc.latestResult.getText()).toEqual('3');
   });
 
-  it('should add four and six', () => {
-    firstNumber.sendKeys(4);
-    secondNumber.sendKeys(6);
+  it('should add four and six', async () => {
+    await calc.setValues(4, 6);
+    calc.goButton.click();
 
-    goButton.click();
-
-    expect(latestResult.getText()).toEqual('10');
+    expect(calc.latestResult.getText()).toEqual('10');
   });
 
   it('should read the value from an input', () => {
-    firstNumber.sendKeys(1);
-    expect(firstNumber.getAttribute('value')).toEqual('1');
+    calc.firstNumber.sendKeys(1);
+    expect(calc.firstNumber.getAttribute('value')).toEqual('1');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,24 @@
-import { browser, by, element } from 'protractor';
+import { ElementFinder, element, by, browser } from 'protractor';
 
-export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+export class CalculatorPage {
+  firstNumber: ElementFinder;
+  secondNumber: ElementFinder;
+  goButton: ElementFinder;
+  latestResult: ElementFinder;
+
+  constructor() {
+    this.firstNumber = element(by.model('first'));
+    this.secondNumber = element(by.model('second'));
+    this.goButton = element(by.id('gobutton'));
+    this.latestResult = element(by.binding('latest'));
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  async navigateTo() {
+    await browser.get('http://juliemr.github.io/protractor-demo/');
+  }
+
+  async setValues(first: number, second: number) {
+    await this.firstNumber.sendKeys(first);
+    await this.secondNumber.sendKeys(second);
   }
 }
