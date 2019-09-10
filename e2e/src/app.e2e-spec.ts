88c8cfb1a41a3f4045a04f8b19e132cfbc7eb78f
Carrie Forde <cforde@bills.com>
2019-09-10 13:43:33 -0700

    Adds example of basic protractor test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,39 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, element, by } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe('Protractor demo app', () => {
+  const firstNumber = element(by.model('first'));
+  const secondNumber = element(by.model('second'));
+  const goButton = element(by.id('gobutton'));
+  const latestResult = element(by.binding('latest'));
 
   beforeEach(() => {
-    page = new AppPage();
+    browser.get('http://juliemr.github.io/protractor-demo/');
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-with-cucumber!');
+  it('should have a title', () => {
+    expect(browser.getTitle()).toEqual('Super Calculator');
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+  it('should add one and two', () => {
+    firstNumber.sendKeys(1);
+    secondNumber.sendKeys(2);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('3');
+  });
+
+  it('should add four and six', () => {
+    firstNumber.sendKeys(4);
+    secondNumber.sendKeys(6);
+
+    goButton.click();
+
+    expect(latestResult.getText()).toEqual('10');
+  });
+
+  it('should read the value from an input', () => {
+    firstNumber.sendKeys(1);
+    expect(firstNumber.getAttribute('value')).toEqual('1');
   });
 });
